8f55bcd527136f0aa478b34f65608bd6f3eeb7ed
bimal-tech <074bct107@nce.edu.np>
2021-12-13 22:15:29 +0545

    Database portion finished

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class operation(models.Model):
+    sn = models.IntegerField()
+    username = models.CharField(max_length=64, null=True, default="none")
+    age = models.IntegerField()
--- crud/views.py
+++ crud/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from .models import *
 # Create your views here.
+
+
+def index(request):
+    datas = operation.objects.all()
+    return render(request, "crud_templates/index.html", {
+        "datas": datas
+    })
+
+
+def add_details(request):
+    if request.method == "POST":
+        item = operation()
+        item.username = request.POST.get('username')
+        item.age = request.POST.get('age')
+        # print(item.sn)
+        # print(item.username)
+        # print(item.age)
+        item.sn = str(len(operation.objects.all())+1)
+        item.save()
+        return redirect('/')
+
+    return render(request, "crud_templates/add_detail.html")
